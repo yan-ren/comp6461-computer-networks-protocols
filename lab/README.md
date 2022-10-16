27ef78175f8b0ba45ecd2acc21ab704c569ffe60
Yan Ren <ryan.ren3@gmail.com>
2022-10-16 01:52:57 -0400

    implement get file list

--- lab/README.md
+++ lab/README.md
@@ -14,33 +14,33 @@
 ```
 #### Get with query parameters
 ```
-./httpc get 'http://httpbin.org/get?course=networking&assignment=1'
+./httpc get 'http://httpbin.org:80/get?course=networking&assignment=1'
 ```
 #### Get with verbose option
 ```
-./httpc get -v 'http://httpbin.org/get?course=networking&assignment=1'
+./httpc get -v 'http://httpbin.org:80/get?course=networking&assignment=1'
 ```
 #### Get with mutiple headers
 ```
-./httpc get -v -h User-Agent:Concordia-HTTP/1.0 -h Host:httpbin.org 'http://httpbin.org/get?course=networking&assignment=1'
+./httpc get -v -h User-Agent:Concordia-HTTP/1.0 -h Host:httpbin.org 'http://httpbin.org:80/get?course=networking&assignment=1'
 ```
 #### Post with inline data
 ```
-./httpc post -h Content-Type:application/json --d '{"Assignment": 1}' http://httpbin.org/post
+./httpc post -h Content-Type:application/json --d '{"Assignment": 1}' http://httpbin.org:80/post
 ```
 #### Post with data file
 ```
-./httpc post -h Content-Type:application/json --f ./data_file.json http://httpbin.org/post
+./httpc post -h Content-Type:application/json --f ./data_file.json http://httpbin.org:80/post
 ```
 #### Optional Task
 #### Output response to file
 ```
-./httpc get -o hello.txt 'http://httpbin.org/get?course=networking&assignment=1' 
+./httpc get -o hello.txt 'http://httpbin.org:80/get?course=networking&assignment=1' 
 ```
 #### Redirection
 ```
-./httpc get 'https://httpbin.org/redirect-to?url=http%3A%2F%2Fhttpbin.org%2Fget%3Fcourse%3Dnetworking%26assignment%3D1'
+./httpc get 'https://httpbin.org:80/redirect-to?url=http%3A%2F%2Fhttpbin.org:80%2Fget%3Fcourse%3Dnetworking%26assignment%3D1'
 ```
 ```
-./httpc post -h Content-Type:application/json --d '{"Assignment": 1}' 'https://httpbin.org/redirect-to?url=http://httpbin.org/post'
+./httpc post -h Content-Type:application/json --d '{"Assignment": 1}' 'https://httpbin.org:80/redirect-to?url=http://httpbin.org:80/post'
 ```
--- lab/httpfs/httpfs.go
+++ lab/httpfs/httpfs.go
@@ -2,23 +2,35 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 const DefaultBufferSize = 1024
 const CRLF = "\r\n"
 const HttpRequestHeaderEnd = "\r\n\r\n"
+const ReadTimeoutDuration = 5 * time.Second
+
+type fileListResponse struct {
+	Files []string `json:"files"`
+}
 
 func main() {
-	port := flag.Int("port", 8007, "echo server port")
+	verbose := flag.Bool("v", false, "verbose mode")
+	// directory := flag.String("d", "", "specifies the directiory that the server will use to read/write")
+	port := flag.Int("p", 8007, "echo server port")
 	flag.Parse()
+
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to listen on %d\n", *port)
@@ -33,51 +45,115 @@ func main() {
 			fmt.Fprintf(os.Stderr, "error occured during accept connection %v\n", err)
 			continue
 		}
-		go handleConn(conn)
+		go handleConn(conn, *verbose)
 	}
 }
 
+func handleGet(conn net.Conn, req *http.Request, verbose bool) {
+	if verbose {
+		fmt.Printf("[debug] handle GET request: %s, path: %s\n", req.URL, req.URL.Path)
+	}
+	if req.URL.Path == "" {
+		cwd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		var respBody fileListResponse
+		respBody.Files = make([]string, 0)
+
+		files, err := ioutil.ReadDir(cwd)
+		if err != nil {
+			fmt.Println(err)
+		}
+		for _, file := range files {
+			if !file.IsDir() {
+				respBody.Files = append(respBody.Files, file.Name())
+			}
+		}
+		b, _ := json.Marshal(respBody)
+
+		// HTTP/1.1 200 OK
+		// Date: Sun, 16 Oct 2022 05:09:56 GMT
+		// Content-Type: application/json
+		// Content-Length: 282
+		res := "HTTP/1.1 200 OK" + "\r\n" +
+			"Date: " + time.Now().UTC().Format(http.TimeFormat) + "\r\n" +
+			"Content-Type: application/json" + "\r\n" +
+			"Content-Length: " + fmt.Sprint(len(string(b))) + HttpRequestHeaderEnd +
+			string(b)
+
+		if _, we := conn.Write([]byte(res)); we != nil {
+			fmt.Fprintf(os.Stderr, "write error %v\n", we)
+		}
+	}
+}
+
+func handlePost(con net.Conn, req *http.Request, verbose bool) {
+	fmt.Println("post method:")
+	b, err := io.ReadAll(req.Body)
+	// b, err := ioutil.ReadAll(resp.Body)  Go.1.15 and earlier
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(b))
+}
+
 //echo reads data and sends back what it received until the channel is closed
-func handleConn(conn net.Conn) {
-	defer conn.Close()
+func handleConn(conn net.Conn, verbose bool) {
+	defer func() {
+		fmt.Printf("closing connection %v\n", conn.RemoteAddr())
+		conn.Close()
+	}()
+
 	fmt.Printf("new connection from %v\n", conn.RemoteAddr())
 
 	buf := make([]byte, DefaultBufferSize)
 	tmp := ""
-	header := ""
+	var request *http.Request
 
 	for {
+		conn.SetReadDeadline(time.Now().Add(ReadTimeoutDuration))
+
 		n, re := conn.Read(buf)
 		if n > 0 {
 			tmp += string(buf[:n])
 			if strings.Contains(tmp, HttpRequestHeaderEnd) {
-				header = tmp[:strings.Index(tmp, HttpRequestHeaderEnd)]
+				header := tmp[:strings.Index(tmp, HttpRequestHeaderEnd)]
+				// parse header
+				reader := bufio.NewReader(strings.NewReader(header + HttpRequestHeaderEnd))
+				req, err := http.ReadRequest(reader)
+				request = req
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
 			}
-			reader := bufio.NewReader(strings.NewReader(header + HttpRequestHeaderEnd))
-			req, err := http.ReadRequest(reader)
-
-			if err != nil {
-				log.Fatal(err)
+			if request != nil && request.Header.Get("Content-Length") == "" {
+				break
+			} else if request != nil && request.Header.Get("Content-Length") == strconv.Itoa(len(tmp)-strings.Index(tmp, HttpRequestHeaderEnd)-len(HttpRequestHeaderEnd)) {
+				// parse body
+				body := ioutil.NopCloser(strings.NewReader(tmp[strings.Index(tmp, HttpRequestHeaderEnd)+len(HttpRequestHeaderEnd):]))
+				request.Body = body
+				break
 			}
-			fmt.Println(req.Method)
-			fmt.Println(req.Header)
-
-			if _, we := conn.Write([]byte("HTTP/1.1 200 OK" + HttpRequestHeaderEnd + "received!!")); we != nil {
-				fmt.Fprintf(os.Stderr, "write error %v\n", we)
-			}
-
-			break
-			// if _, we := conn.Write(buf[:n]); we != nil {
-			// 	fmt.Fprintf(os.Stderr, "write error %v\n", we)
-			// 	break
-			// }
 		}
 		if re == io.EOF {
-			break
+			return
 		}
 		if re != nil {
 			fmt.Fprintf(os.Stderr, "read error %v\n", re)
-			break
+			return
 		}
+	}
+
+	// fmt.Println(req.Method)
+
+	if request.Method == http.MethodGet {
+		handleGet(conn, request, verbose)
+	}
+
+	if request.Method == http.MethodPost {
+		handlePost(conn, request, verbose)
 	}
 }
